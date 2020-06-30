eaf7154d1aab4a53d609acdb9efc89ec20139c47
X͜͡๖ۣۜBEAN々NoReal <66841000+irfanfebrian13@users.noreply.github.com>
2020-06-30 10:38:10 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -131,12 +131,12 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     logo = ALIVE_LOGO
-    output = (f"`XBeanNoreal` is running on `{UPSTREAM_REPO_BRANCH}`\n"
-             f"====================================\n"
-             f"🐍 `Python    :` v{python_version()}\n"
-             f"⚙️ `Telethon  :` v{version.__version__}\n"
-             f"👤 `User      :` {DEFAULTUSER}\n"
-             f"====================================\n")
+    output = (f"`XBeanNoreal` is running on `{UPSTREAM_REPO_BRANCH}`\n`COPYRIGHT` (C) `2019` ***RAPHIELSCAPE*** "
+             f"┏━━━━━━━━━━━━━━━━━━━\n"
+             f"┣[ 🐍 `Python    :` v{python_version()}\n"
+             f"┣[ ⚙️ `Telethon  :` v{version.__version__}\n"
+             f"┣[ 👤 `User      :` {DEFAULTUSER}\n"
+             f"┗━━━━━━━━━━━━━━━━━━━\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
 
